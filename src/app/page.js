39587eb69fb2a364c8e61d700d34703f7e1da746
shameem-shem <shemfolio@gmail.com>
2023-10-15 15:49:06 +0530

    env remve next config

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
-// 'use client';
-// import { useEffect } from "react";
-// import Image from 'next/image'
 import Banner from "@/components/banner";
 import Treasury from "@/components/treasury";
 import About from "@/components/about";
@@ -9,20 +6,15 @@ import Features from "@/components/features";
 import FAQ from "@/components/faq";
 import RecentTweeks from "@/components/recentTweeks";
 import { getData, treasury, twitterFeedsIds } from "@/data";
-// import { useRouter } from "next/navigation";
 
 export default async function Home() {
-    // const router = useRouter()
     const tweets = await getData("twitter");
-    // const treasury = await getData("treasury");
-    // console.log( router); 
-    // useEffect(()=>{
-    //     console.log(JSON.stringify(twitterFeedsIds))
-    // },[])
+    const treasury = await getData("treasury");
+   
     return (
         <main className="">
             <Banner />
-            {/* <Treasury treasury={treasury} />  */}
+            <Treasury treasury={treasury} /> 
             <About />
             <Statistics />
             <Features />
