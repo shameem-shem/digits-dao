2f77d61f6c661f1d907a817e6d45a22113e09feb
shameem-shem <shemfolio@gmail.com>
2023-10-15 12:21:13 +0530

    stringify object

--- src/app/api/twitter/route.js
+++ src/app/api/twitter/route.js
@@ -3,5 +3,5 @@ import { twitterFeedsIds } from "@/data";
 
 export async function GET(request) {
     // console.log(request,NextResponse.json({status:400},{ data: twitterFeedsIds }))
-    return NextResponse.json({ data: twitterFeedsIds });
+    return NextResponse.json({ data: twitterFeedsIds.toString() });
 }
