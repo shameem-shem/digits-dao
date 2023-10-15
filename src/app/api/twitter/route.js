e5ed5cbc313fd7970b60a5a9f94023aa5593ee20
shameem-shem <shemfolio@gmail.com>
2023-10-15 20:35:59 +0530

    xax

--- src/app/api/twitter/route.js
+++ src/app/api/twitter/route.js
@@ -1,9 +1,15 @@
 import { NextResponse } from "next/server";
 import { twitterFeedsIds } from "@/data";
 
+
+function removeByteOrderMark(str){
+    return str.replace(/^\ufeff/g,"")
+}
+
 export async function GET(request) {
+    const twitterData = removeByteOrderMark(JSON.stringify(twitterFeedsIds))
     // console.log(request,NextResponse.json({status:400},{ data: twitterFeedsIds }))
-    return NextResponse.json(JSON.stringify({ data: twitterFeedsIds }), {
+    return NextResponse.json(JSON.stringify({ data: twitterData }), {
         headers: { 'content-type': 'application/json' },
       });
 }
