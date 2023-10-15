eadd25ac2f139ca1b639cb24deab3176302d8514
shameem-shem <shemfolio@gmail.com>
2023-10-15 20:44:21 +0530

    develop build fix

--- src/app/api/twitter/route.js
+++ src/app/api/twitter/route.js
@@ -1,18 +1,6 @@
 import { NextResponse } from "next/server";
 import { twitterFeedsIds } from "@/data";
 
-
-function removeByteOrderMark(str){
-    return str.replace(/^\ufeff/g,"")
-}
-
 export async function GET(request) {
-    const twitterData = removeByteOrderMark(JSON.stringify(twitterFeedsIds))
-    // console.log(request,NextResponse.json({status:400},{ data: twitterFeedsIds }))
-    return NextResponse.json(JSON.stringify({ data: twitterData }), {
-        headers: { 'content-type': 'application/json' },
-      });
+    return NextResponse.json({ data: twitterFeedsIds });
 }
-// export  function handler(req, res) {
-//     res.status(200).json({ text: 'Hello' });
-// }
